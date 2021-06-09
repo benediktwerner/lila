27c3a8fbc90061eccd870a13bd8667da51cd97b7
Benedikt Werner <1benediktwerner@gmail.com>
2021-06-09 13:09:15 +0200

    50% sounds

--- ui/speech/src/main.ts
+++ ui/speech/src/main.ts
@@ -9,43 +9,46 @@ const soundNames = [
   'takes',
   'check',
   'checkmate',
-  'promote',
+  'promotesTo',
   'at',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
   'pawn',
   'rook',
   'knight',
   'bishop',
   'queen',
   'king',
-];
+] as const;
 
-type SoundName = typeof soundNames[number];
+type SoundName = typeof soundNames[number] | Files | `${Files}${Ranks}`;
 
 let playing: SoundName | undefined;
 let queue: SoundName[] = [];
 
-const sounds: Record<SoundName, Howl> = soundNames.reduce((o, sound) => {
-  o[sound] = new window.Howl({
+const sounds: Record<SoundName, Howl> = loadSounds();
+
+function loadSound(sound: SoundName) {
+  if (sound.length === 1 || (sound.length === 2 && sound[0] > 'e'))
+    return new window.Howl({
+      src: ['ogg', 'mp3'].map(ext => `${lichess.sound.baseUrl}/Silence.${ext}`),
+    });
+
+  return new window.Howl({
     src: ['ogg', 'mp3'].map(ext => `${lichess.sound.baseUrl}/voice/${sound}.${ext}`),
   });
-  return o;
-}, {} as Record<SoundName, Howl>);
+}
+
+function loadSounds() {
+  const sounds = {} as Record<SoundName, Howl>;
+  soundNames.forEach(sound => (sounds[sound] = loadSound(sound)));
+  for (const letter of ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] as const) {
+    sounds[letter] = loadSound(letter);
+    for (let num = 1; num <= 8; num++) {
+      const sound = (letter + num) as SoundName;
+      sounds[sound] = loadSound(sound);
+    }
+  }
+  return sounds;
+}
 
 function play(sound: SoundName) {
   queue.push(sound);
@@ -57,13 +60,17 @@ function playNext() {
   if (playing) {
     const howl = sounds[playing];
     lichess.sound.playHowl(howl);
-    setTimeout(() => playNext(), howl._duration * 1000);
+    setTimeout(() => playNext(), howl._duration * 1000 - 50);
   }
 }
 
 function isCoord(c: string): c is SoundName {
-  const code = c.charCodeAt(0);
-  return (code > 48 && code < 58) || (code > 96 && code < 105);
+  if (c.length !== 2) return false;
+  return isFile(c[0]) && '1' <= c[0] && c[1] <= '8';
+}
+
+function isFile(c: string): c is SoundName {
+  return 'a' <= c[0] && c[0] <= 'h';
 }
 
 function renderSan(san: San, cut?: boolean) {
@@ -79,13 +86,18 @@ function renderSan(san: San, cut?: boolean) {
     if (san.endsWith('+')) play('check');
     else if (san.endsWith('#')) play('checkmate');
   } else
-    san.split('').forEach(c => {
-      // prettier-ignore
-      switch (c) {
+    san
+      .split('')
+      .join(' ')
+      .replace(/([a-h]) ([1-8])/, '$1$2')
+      .split(' ')
+      .forEach(c => {
+        // prettier-ignore
+        switch (c) {
         case 'x': return play('takes');
         case '+': return play('check');
         case '#': return play('checkmate');
-        case '=': return play('promote');
+        case '=': return play('promotesTo');
         case '@': return play('at');
         case 'P': return play('pawn');
         case 'R': return play('rook');
@@ -93,9 +105,9 @@ function renderSan(san: San, cut?: boolean) {
         case 'B': return play('bishop');
         case 'Q': return play('queen');
         case 'K': return play('king');
-        default: if (isCoord(c)) play(c);
+        default: if (isFile(c) || isCoord(c)) play(c);
       }
-    });
+      });
 }
 
 export function step(s: { san?: San }, cut?: boolean) {
